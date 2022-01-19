d64424cc0cbea69d6d35927a28307dc3e0ce1f15
CMD098 <97360169+cmd098@users.noreply.github.com>
2022-01-18 20:53:17 -0600

    Add beat_pattern.py to visualize wave beats

---
+++ beat_pattern.py
@@ -0,0 +1,73 @@
+import argparse
+from numpy import *
+import matplotlib.pyplot as plt
+
+
+def eval_sine_wave(amplitude, angular_frequency, time):
+    # evaluates y = Asin(-ωt)
+    return amplitude * sin(angular_frequency * time)
+
+
+def get_angular_frequency(frequency):
+    # evaluates ω = 2πf
+    return (2 * pi) * frequency
+
+
+f1, f2 = 20, 22  # wave frequencies in Hz
+time_points = arange(0, 2, 0.001)  # points to evaluate function
+wave1 = eval_sine_wave(10, get_angular_frequency(f1), time_points)
+wave2 = eval_sine_wave(10, get_angular_frequency(f2), time_points)
+
+# - - Wave plotting - -
+
+fig, axs = plt.subplots(
+    4, sharex='all', figsize=(12, 7),
+    gridspec_kw={'height_ratios': [2, 2, 2, 4]}
+)
+
+# title, subtitle, axis labels
+fig.suptitle(
+    'Demonstration of beat patterns as a product of wave interference',
+    size=14
+)
+fig.text(0.5, 0.025, 'Time (ms)', ha='center')
+fig.text(0.008, 0.54, 'Amplitude (m)', va='center', rotation='vertical')
+fig.text(
+    0.5, 0.92, 'A beat pattern is produced by the superposition of two waves '
+    'of slightly different frequencies', ha='center''', size=9
+)
+fig.tight_layout(pad=2.8)
+
+# subplot - wave 1
+axs[0].plot(wave1, color='#e23654')
+axs[0].set_title(
+    f'f1 = {f1:.2f} Hz; ω1 = {get_angular_frequency(f1):.2f} rad/s',
+    loc='left', size=11
+)
+
+# subplot - wave 2
+axs[1].plot(wave2, color='#627ddb')
+axs[1].set_title(
+    f'f2 = {f2:.2f} Hz; ω2 = {get_angular_frequency(f2):.2f} rad/s ',
+    loc='left', size=11
+)
+
+# subplot - wave superposition
+axs[2].plot(wave1, color='#e23654')
+axs[2].plot(wave2, color='#627ddb')
+axs[2].set_title('Superposition (f1+f2)', loc='left', size=11)
+
+# subplot - resultant pattern
+axs[3].plot(wave1+wave2, color='#635e9d')
+axs[3].set_title(
+    f'Resultant beat pattern (fb = {abs(f1-f2):.2f} Hz; '
+    f'ωb = {get_angular_frequency(abs(f1-f2)):.2f} rad/s)',
+    loc='left', size=11
+)
+
+for ax in axs:
+    ax.yaxis.set_major_formatter(lambda tick, position: str(abs(tick)))
+    ax.grid()
+
+plt.savefig('beat_pattern.png')
+plt.show()
